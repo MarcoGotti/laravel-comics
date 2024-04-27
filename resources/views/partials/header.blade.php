c31d98ac3b1c7cc9faeaaa2faf09c0aabe15015e
MarcoGotti <mrc.gotti@gmail.com>
2024-04-27 12:16:26 +0000

    header MarkUp

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,37 @@
-<header>header</header>
+<header>
+    <section id="top-line">
+        <div class="container top-line-container">
+            <div class="top-banners">
+                <span>dc power visa</span>
+                <span>additional dc sites</span>
+            </div>
+        </div>
+    </section>
+    <section id="header-main" class="container">
+        
+          <div class="logo">
+            <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
+          </div>
+          <ul>
+            <li><a href="">Characters</a></li>
+            <li><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Collectibles</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">Fans</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Shop</a></li>
+          </ul>         
+          <input type="text" id="search" placeholder="Search">
+          
+        
+    </section>
+    <section id="jumbotron">
+        <div class="jumbotron"></div>
+    </section>
+    
+
+      
+</header>
